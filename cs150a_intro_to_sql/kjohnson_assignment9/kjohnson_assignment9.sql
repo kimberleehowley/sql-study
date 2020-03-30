96b76196775a5dbd19314c2469e5de5067f39ee6
Kimberlee Johnson <kimee.johnson@gmail.com>
2020-03-29 20:41:55 -0700

    Used subquery to mimic results of a join

--- cs150a_intro_to_sql/kjohnson_assignment9/kjohnson_assignment9.sql
+++ cs150a_intro_to_sql/kjohnson_assignment9/kjohnson_assignment9.sql
@@ -1,3 +1,12 @@
 /* kjohnson_assignment9.sql
    Kimberlee Johnson
    CS 150A, Spring 2020 */
+
+/* Query 0 */
+USE bookstore; 
+
+/* Query 1 */
+SELECT DISTINCT category_name 
+FROM category c 
+WHERE c.category_id IN (SELECT DISTINCT category_id FROM product)
+ORDER BY category_name; 
