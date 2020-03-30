66dedd96a46be895dbeb83a1ff3348071d036bb1
Kimberlee Johnson <kimee.johnson@gmail.com>
2020-03-29 20:49:00 -0700

    Used a subquery to return product prices greater than an average

--- cs150a_intro_to_sql/kjohnson_assignment9/kjohnson_assignment9.sql
+++ cs150a_intro_to_sql/kjohnson_assignment9/kjohnson_assignment9.sql
@@ -10,3 +10,9 @@ SELECT DISTINCT category_name
 FROM category c 
 WHERE c.category_id IN (SELECT DISTINCT category_id FROM product)
 ORDER BY category_name; 
+
+/* Query 1 */
+SELECT product_name, list_price 
+FROM product 
+WHERE list_price > ALL (SELECT AVG(list_price) FROM product)
+ORDER BY list_price DESC;
