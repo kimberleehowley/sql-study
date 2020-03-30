c19f15147893fdbfa5145777fe1468cf28099f9f
Kimberlee Johnson <kimee.johnson@gmail.com>
2020-03-29 20:54:09 -0700

    Used EXISTS to limit results

--- cs150a_intro_to_sql/kjohnson_assignment9/kjohnson_assignment9.sql
+++ cs150a_intro_to_sql/kjohnson_assignment9/kjohnson_assignment9.sql
@@ -8,11 +8,24 @@ USE bookstore;
 /* Query 1 */
 SELECT DISTINCT category_name 
 FROM category c 
-WHERE c.category_id IN (SELECT DISTINCT category_id FROM product)
+WHERE c.category_id IN 
+   (SELECT DISTINCT category_id 
+    FROM product)
 ORDER BY category_name; 
 
-/* Query 1 */
+/* Query 2 */
 SELECT product_name, list_price 
 FROM product 
-WHERE list_price > ALL (SELECT AVG(list_price) FROM product)
+WHERE list_price > ALL 
+   (SELECT 
+    AVG(list_price) 
+    FROM product)
 ORDER BY list_price DESC;
+
+/* Query 3 */
+ SELECT category_name
+ FROM category c
+ WHERE EXISTS 
+	(SELECT *
+     FROM product 
+     WHERE category_id = c.category_id); 
