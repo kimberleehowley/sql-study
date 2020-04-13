951a1f6766a275d80bf0a5246c53b509594d38ef
Kimberlee Johnson <kimee.johnson@gmail.com>
2020-04-12 19:27:19 -0700

    notes: Added Intro to document databases notes

--- cs150a_intro_to_sql/kjohnson_assignment11/kjohnson_assignment11.sql
+++ cs150a_intro_to_sql/kjohnson_assignment11/kjohnson_assignment11.sql
@@ -1,1 +1,7 @@
 /* NoSQL Systems */
+
+-- MongoDB is a cross-platform document database. 
+-- A document is a set of key:value pairs 
+-- BSON format can easily be converted to JSON, used in Node.js app 
+-- Multiple attributes are organized in a single object 
+-- Order of objects matters for documents 
